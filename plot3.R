4083044e739b069de81010331ada89b935412236
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-10-07 18:17:50 +0530

    plot to png

--- plot3.R
+++ plot3.R
@@ -8,5 +8,14 @@ plot_data<-as.data.frame(with(data1[which(data1$fips=="24510"),],
           mutate(year=as.numeric(rownames(.))) %>%
           gather(type,value,1:4)
 
+#plot 3
+png("plot3.png")
+ggplot(plot_var,aes(year,value))+
+  geom_line()+
+  facet_wrap(.~type, scales="free_y")+
+  geom_line(data=plot_var[which(plot_var$year==1999|plot_var$year==2008),],
+            aes(year,value),linetype=2,size=.5,col="red")+
+  labs(ylab="Total PM2.5 emissions in tons")
+dev.off()
   
   

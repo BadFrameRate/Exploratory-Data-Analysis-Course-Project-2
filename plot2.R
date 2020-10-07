6b91d9d96db05729c81b8f082fb37c5ab7c8d3e6
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-10-07 18:53:18 +0530

    a ilttle formatting

--- plot2.R
+++ plot2.R
@@ -4,7 +4,8 @@ scc_detail<-readRDS("./Source_Classification_Code.rds")
 
 
 #calculate total emissions for each year in fips=24510
-totemissions24510<-with(pm25data[which(pm25data$fips=="24510"),],tapply(Emissions,year,sum))
+totemissions24510<-with(pm25data[which(pm25data$fips=="24510"),],
+                        tapply(Emissions, year, sum))
 
 #plot 2
 options(scipen=10)

68ed7e5d3760d9568578cb395e57c13885410ee7
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-10-07 15:38:12 +0530

    calculate and plot to png

--- plot2.R
+++ plot2.R
@@ -2,3 +2,13 @@
 pm25data<-readRDS("./summarySCC_PM25.rds")
 scc_detail<-readRDS("./Source_Classification_Code.rds")
 
+
+#calculate total emissions for each year in fips=24510
+totemissions24510<-with(data1[which(data1$fips=="24510"),],tapply(Emissions,year,sum))
+
+#plot 2
+options(scipen=10)
+png("plot2.png")
+plot(names(totemissions24510), totemissions24510, typ="l", col="red", lwd=2, 
+     ylab="Total PM2.5 emmissions in tons", xlab="Year")
+dev.off()

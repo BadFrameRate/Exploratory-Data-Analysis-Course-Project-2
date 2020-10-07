2dd18aa5ca015d5e1f9b6a8367e14043609df4a2
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-10-07 18:17:28 +0530

    req calc for plot creation

--- plot3.R
+++ plot3.R
@@ -2,3 +2,11 @@
 pm25data<-readRDS("./summarySCC_PM25.rds")
 scc_detail<-readRDS("./Source_Classification_Code.rds")
 
+#calculating and modifying data for plotting
+plot_data<-as.data.frame(with(data1[which(data1$fips=="24510"),],
+               tapply(Emissions, list(year, type), sum))) %>%
+          mutate(year=as.numeric(rownames(.))) %>%
+          gather(type,value,1:4)
+
+  
+  

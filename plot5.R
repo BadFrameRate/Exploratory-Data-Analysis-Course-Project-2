e242ede6e83e1d5d80c52d7d3805b071d86e6290
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-10-07 20:05:27 +0530

    plot to png

--- plot5.R
+++ plot5.R
@@ -1,3 +1,14 @@
 #Read data files
 pm25data<-readRDS("./summarySCC_PM25.rds")
 scc_detail<-readRDS("./Source_Classification_Code.rds")
+
+#calculating data for plotting
+sources2<-scc_detail[grep("Mobile.*Road|Mobile.*Comm",levels(scc_detail$EI.Sector)),"SCC"]
+tot_emissions24510<-with(pm25data[pm25data$SCC %in% sources2 & which(pm25data$fips=="24510"),],
+                         tapply(Emissions,year,sum))
+
+#plot 5
+png("plot5.png")
+plot(names(tot_emissions24510), tot_emissions24510, typ="l", col="red", lwd=2, 
+     ylab="Total PM2.5 emmissions in tons", xlab="Year")
+dev.off()

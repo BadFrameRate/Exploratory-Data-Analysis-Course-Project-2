517bc31d99dadb58fabfbcf49be9822faba24737
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-10-07 18:23:15 +0530

    fixed tyops

--- plot3.R
+++ plot3.R
@@ -10,12 +10,10 @@ plot_data<-as.data.frame(with(data1[which(data1$fips=="24510"),],
 
 #plot 3
 png("plot3.png")
-ggplot(plot_var,aes(year,value))+
+ggplot(plot_data,aes(year,value))+
   geom_line()+
   facet_wrap(.~type, scales="free_y")+
-  geom_line(data=plot_var[which(plot_var$year==1999|plot_var$year==2008),],
+  geom_line(data=plot_data[which(plot_data$year==1999|plot_data$year==2008),],
             aes(year,value),linetype=2,size=.5,col="red")+
-  labs(ylab="Total PM2.5 emissions in tons")
+  labs(y="Total PM2.5 emissions in tons")
 dev.off()
-  
-  

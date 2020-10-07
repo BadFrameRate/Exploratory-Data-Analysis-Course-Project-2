446ff4dbb59c87e62449a717c029d40d47b30356
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-10-07 15:16:14 +0530

    calculation and plot

--- plot1.R
+++ plot1.R
@@ -2,3 +2,9 @@
 pm25data<-readRDS("./summarySCC_PM25.rds")
 scc_detail<-readRDS("./Source_Classification_Code.rds")
 
+#calculate total emissions for each year
+total_emissions<-with(pm25data, tapply(Emissions, year, sum,))
+
+#plot 1
+plot(names(total_emissions), total_emissions, typ="l", col="red", lwd=2, 
+     ylab="Total PM2.5 emmissions in tons", xlab="Year")

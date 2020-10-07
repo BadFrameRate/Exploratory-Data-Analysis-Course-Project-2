a29a039409faddaea468cc96630075606876766b
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-10-07 22:08:58 +0530

    req calc for plot creation

--- plot6.R
+++ plot6.R
@@ -1,3 +1,13 @@
 #Read data files
 pm25data<-readRDS("./summarySCC_PM25.rds")
 scc_detail<-readRDS("./Source_Classification_Code.rds")
+
+#calculating and modifying data for plotting
+sources2<-scc_detail[grep("Motor|Vehicle",scc_detail$Short.Name,ignore.case = T),"SCC"]
+Baltimore_city<-pm25data %>% subset(.,SCC %in% sources2) %>% subset(.,fips=="24510") %>%
+                with(.,tapply(Emissions, year, sum))
+Los_Angeles_County<-pm25data %>% subset(.,SCC %in% sources2) %>% subset(.,fips=="06037") %>%
+                    with(.,tapply(Emissions, year, sum))
+plot_data<-data.frame(Baltimore_city, Los_Angeles_County, year=c(1999,2002,2005,2008)) %>%
+           gather(city, value, 1:2)
+

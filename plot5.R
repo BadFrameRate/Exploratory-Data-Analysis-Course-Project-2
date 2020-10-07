30a32dac154d28f5fab0171ca0388a70face8ab1
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-10-07 22:07:23 +0530

    revised subsetting technique

--- plot5.R
+++ plot5.R
@@ -3,9 +3,9 @@ pm25data<-readRDS("./summarySCC_PM25.rds")
 scc_detail<-readRDS("./Source_Classification_Code.rds")
 
 #calculating data for plotting
-sources2<-scc_detail[grep("Motor|motor",scc_detail$Short.Name), "SCC"]
-tot_emissions24510<-with(pm25data[pm25data$SCC %in% sources2 & which(pm25data$fips=="24510"),],
-                         tapply(Emissions, year, sum))
+sources2<-scc_detail[grep("Motor|vehicle", scc_detail$Short.Name, ignore.case=T), "SCC"]
+tot_emissions24510<-pm25data %>% subset(., SCC %in% sources2) %>% subset(., fips=="24510") %>%
+                    with(., tapply(Emissions, year, sum))
 
 #plot 5
 png("plot5.png")
